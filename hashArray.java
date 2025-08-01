8f0c4a21a8fc457174597b8bb421f97ac04638e1
Anuragjha0936 <anuragjha4032@gmail.com>
2025-08-02 00:47:20 +0530

    a

--- hashArray.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-public class hashArray {
-    public static void main(String[] args) {
-       int[] arr={1,2,1,3};
-        HashMap<Integer,Integer> map=new HashMap<>();
-        
-    }
-}
