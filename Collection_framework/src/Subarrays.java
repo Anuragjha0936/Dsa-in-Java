830b3e0d4da7a626acc816aa50062022eef6dcbd
Anuragjha0936 <anuragjha4032@gmail.com>
2025-08-02 15:23:27 +0530

    DSA

--- Collection_framework/src/Subarrays.java
+++ Collection_framework/src/Subarrays.java
@@ -5,7 +5,6 @@ public class Subarrays {
         int[] arr={10,2,-2,-20,10};
         int k=-10;
         int sum=0;
-        int sumj=0;
         HashMap<Integer,Integer> map=new HashMap<>();
         int ans=0;
         map.put(0,1);
