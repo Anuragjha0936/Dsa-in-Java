667e23c1d45d5b60ae819d2a2e80db5cc202bf3b
Anuragjha0936 <anuragjha4032@gmail.com>
2026-02-06 01:47:30 +0530

    DSA

---
+++ Pattern/src/Inc_letter_triangle.java
@@ -0,0 +1,23 @@
+public class Inc_letter_triangle {
+    public static void main(String[] args) {
+        int n=5;
+        int ch='A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)(ch)+" ");
+                ch++;
+            }
+            System.out.println();
+            ch='A';
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print((char)(ch)+" ");
+                ch++;
+            }
+            System.out.println();
+            ch='A';
+        }
+    }
+}
