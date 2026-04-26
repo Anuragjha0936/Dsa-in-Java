f4d47479caf64f3f468a4fc94aa81c1ee30061b2
Anuragjha0936 <anuragjha4032@gmail.com>
2026-04-27 01:30:29 +0530

    DSA

---
+++ Backtracking/src/N_Queens.java
@@ -0,0 +1,81 @@
+public class N_Queens {
+    public static void main(String[] args) {
+        int n=1;
+        char[][] board=new char[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='X';
+            }
+        }
+        func(board,0);
+    }
+    private static void func(char[][] board,int row){
+        if(row== board.length){
+            for(int i=0;i<board.length;i++){
+                for(int j=0;j<board[0].length;j++){
+                    System.out.print(board[i][j]);
+                }
+                System.out.println();
+                }
+            System.out.println();
+            return;
+            }
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+                func(board,row+1);
+                board[row][j]='X';
+            }
+        }
+    }
+
+    private static boolean isSafe(char[][] board,int row,int col){
+//        for row
+        for(int j=0;j< board.length;j++){
+            if(board[row][j]=='Q') return false;
+        }
+//        for col
+        for(int i=0;i< board.length;i++){
+            if(board[i][col]=='Q') return false;
+        }
+
+        int temp_row=row;
+        int temp_col=col;
+//        for north-west diagonal
+        while(temp_row >=0 && temp_col >=0){
+            if(board[temp_row][temp_col]=='Q') return false;
+            temp_row--;
+            temp_col--;
+        }
+
+        temp_row=row;
+        temp_col=col;
+//        North-east diagonal
+        while(temp_row >=0 && temp_col <= board.length-1){
+            if(board[temp_row][temp_col]=='Q') return false;
+            temp_row--;
+            temp_col++;
+        }
+
+        temp_row=row;
+        temp_col=col;
+//     South-west diagonal
+        while(temp_row <= board.length-1 && temp_col >=0){
+            if(board[temp_row][temp_col]=='Q') return false;
+            temp_row++;
+            temp_col--;
+        }
+
+        temp_row=row;
+        temp_col=col;
+//        south-east diagonal
+        while(temp_row <= board.length-1 && temp_col<= board.length-1){
+            if(board[temp_row][temp_col]=='Q') return false;
+            temp_row++;
+            temp_col++;
+        }
+
+        return true;
+    }
+}
